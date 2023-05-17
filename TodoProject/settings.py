dc5ec31bd1823bb6ed3e5fc259c20676317b1a3b
Fawas <fawaskp1010@gmail.com>
2023-05-17 16:55:19 +0530

    static file dir settings added

--- TodoProject/settings.py
+++ TodoProject/settings.py
@@ -116,6 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+MEDIA_ROOT = BASE_DIR / 'uploads'
+
+MEDIA_URL = '/media/'
 STATIC_URL = 'static/'
 
 # Default primary key field type
