a7241bf63c2a645efc3e07371ddbe940255448f7
Fawas <fawaskp1010@gmail.com>
2023-05-17 21:23:35 +0530

    backend basic functionalites added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Tasks(models.Model):
+    session_id = models.CharField(max_length=20)
+    added_time = models.DateTimeField(auto_created=True)
+    do_task = models.CharField(max_length=300)
--- home/views.py
+++ home/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.sessions.models import Session
+from .models import Tasks
 
-def app(req):
-    return render(req,'test.html')
 
-def add_do(request):
-    return None
+
+def home(request):
+    session_id = request.COOKIES.get('sessionid')
+    tasks=[]
+
+    tasks = Tasks.objects.filter(session_id=session_id)
+    return render(request,'test.html',{'tasks':tasks})
+
+
+def add_do_task(request):
+
+    if request.method == 'POST':
+        session_id = request.COOKIES.get('sessionid')
+        todo_task = request.POST['todo_task']
+        Tasks.objects.create(session_id=session_id,do_task=todo_task)
+        return redirect(home)
+
+    return HttpResponse('Request Failed! Unauthorised Access')
