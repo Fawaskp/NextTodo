55aa96e5265160ad7d46f60937062988af30e287
Fawas <fawaskp1010@gmail.com>
2023-05-18 10:38:15 +0530

    backend basic working completed

--- home/models.py
+++ home/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Tasks(models.Model):
-    session_id = models.CharField(max_length=20)
-    added_time = models.DateTimeField(auto_created=True)
+    user_id = models.CharField(max_length=20)
+    added_time = models.DateTimeField(auto_now_add=True)
     do_task = models.CharField(max_length=300)
--- home/views.py
+++ home/views.py
@@ -2,23 +2,36 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.sessions.models import Session
 from .models import Tasks
+from django.contrib.sessions.backends.db import SessionStore
+import uuid
 
+def create_user_session(request, user_id):
+    session = SessionStore()
+    session['user_id'] = str(user_id)
+    session.create()
+    request.session = session
+    return user_id
 
 
 def home(request):
-    session_id = request.COOKIES.get('sessionid')
+    user_id = request.session.get('user_id')
     tasks=[]
 
-    tasks = Tasks.objects.filter(session_id=session_id)
+    tasks = Tasks.objects.filter(user_id=user_id)
     return render(request,'test.html',{'tasks':tasks})
 
 
 def add_todo_task(request):
 
     if request.method == 'POST':
-        session_id = request.COOKIES.get('sessionid')
+
+        user_id = request.session.get('user_id')
+        if not user_id:
+            user_id = create_user_session(request,uuid.uuid4())
+
+        print('user id = ',user_id)
         todo_task = request.POST['todo_task']
-        Tasks.objects.create(session_id=session_id,do_task=todo_task)
+        Tasks.objects.create(user_id=user_id,do_task=todo_task)
         return redirect(home)
 
     return HttpResponse('Request Failed! Unauthorised Access')
